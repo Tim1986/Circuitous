61e4e87bdd62f2ad1ce227bbaaa5945a21714210
Max Szczepaniak <wookslandline@protonmail.com>
2019-07-18 21:22:53 -0400

    grabbed variables for dynamic tab creation and researched more in the docs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,34 @@
 var ctx = $('#chart-canvas')
 var ctx = document.getElementById('chart-canvas').getContext('2d');
 
+// grabbing form variables
+var activityName = $('activity-name').val()
+var startTime = $('start-time').val()
+var endTime = $
+
+
+function pushData(){
+    data.labels.append(activityName)
+
+}
+
+var data ={
+    labels: [],
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 00, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+}
+
+
+
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'pie',
     // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+    data: data,
 
     // Configuration options go here
     options: {
@@ -26,7 +41,7 @@ var chart = new Chart(ctx, {
                 right: '50px',
                 top: '50px',
                 bottom: '50px',
-            }
+            },
         }
     }
-});
+})
