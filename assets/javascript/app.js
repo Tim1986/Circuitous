31b1fcbcbe3bd0cc8b3365e205b56660635b8e44
Cmack704 <craigmack704@gmail.com>
2019-07-20 21:48:16 -0400

    added views and add event clicks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -190,20 +190,3 @@ $(document).ready(function () {
 
 })
 
-document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar-goes-here');
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: ['dayGrid', 'interaction', 'list', 'moment'],
-        header: { center: 'dayGridMonth, dayGridWeek, dayGridDay' }, // buttons for switching between views
-
-        views: {
-            dayGridMonth: { // name of view
-                titleFormat: { year: 'numeric', month: '2-digit', day: '2-digit' }
-                // other view-specific options here
-            }
-        }
-    });
-
-    calendar.render();
-});
